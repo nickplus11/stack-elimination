2b4a03f6e68871990b575b71cd6bc4a568e5fe8a
Nikolay Plyusnin <nikolay.plyusnin@jetbrains.com>
2021-09-14 20:47:29 +0300

    implement stack with elimination optimization

--- src/main/java/stack/StackImpl.java
+++ src/main/java/stack/StackImpl.java
@@ -2,6 +2,8 @@ package stack;
 
 import kotlinx.atomicfu.AtomicRef;
 
+import java.util.ArrayList;
+
 public class StackImpl implements Stack {
     private static class Node {
         final AtomicRef<Node> next;
@@ -13,19 +15,64 @@ public class StackImpl implements Stack {
         }
     }
 
+    public static final int eliminationArraySize = 512;
+    public static final int eliminationArrayCheckTimes = 64;
     // head pointer
     private AtomicRef<Node> head = new AtomicRef<>(null);
 
+    private ArrayList<AtomicRef<Integer>> eliminationArray = new ArrayList<>(eliminationArraySize);
+
     @Override
     public void push(int x) {
-        head.setValue(new Node(x, head.getValue()));
+        while (true) {
+            int tryAddIndex = tryAddNewNode(x);
+
+            if (tryAddIndex >= 0) {
+                for (int i = 0; i < eliminationArrayCheckTimes; ++i) {
+                    if (eliminationArray.get(tryAddIndex) == null) return;
+                }
+            }
+
+            Node curHead = head.getValue();
+            Node newNode = new Node(x, curHead);
+            if (head.compareAndSet(curHead, newNode)) return;
+        }
     }
 
     @Override
     public int pop() {
-        Node curHead = head.getValue();
-        if (curHead == null) return Integer.MIN_VALUE;
-        head.setValue(curHead.next.getValue());
-        return curHead.x;
+        while (true) {
+            Integer anyNotNullNode = tryRemoveAnyNotNullNode();
+            if (anyNotNullNode != null) return anyNotNullNode;
+
+            Node curHead = head.getValue();
+            if (curHead == null) return Integer.MIN_VALUE;
+
+            if (head.compareAndSet(curHead, curHead.next.getValue())) {
+                return curHead.x;
+            }
+        }
+    }
+
+    private int tryAddNewNode(int x) {
+        for (int i = 0; i < eliminationArray.size(); ++i) {
+            Integer curElem = eliminationArray.get(i).getValue();
+            if (curElem != null) {
+                eliminationArray.get(i).compareAndSet(curElem, x);
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private Integer tryRemoveAnyNotNullNode() {
+        for (int i = 0; i < eliminationArray.size(); ++i) {
+            Integer curElem = eliminationArray.get(i).getValue();
+            if (curElem != null) {
+                eliminationArray.get(i).compareAndSet(curElem, null);
+                return curElem;
+            }
+        }
+        return null;
     }
 }
